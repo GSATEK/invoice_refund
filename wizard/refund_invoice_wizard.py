17f728ef7e5b9df38278ab50c40574090390799d
Jesús Briceño <jesusbingucv@gmail.com>
2024-12-16 18:29:40 -0400

    [ADD] wizard skeleton with request prof concept

---
+++ wizard/refund_invoice_wizard.py
@@ -0,0 +1,10 @@
+import requests
+
+#Request prof concept for refund invoice
+#api_keuy = secret_api_key
+#url = https://api.stripe.com/v1/refunds
+#headers = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": f"Bearer {api_key}"}
+#charge = charge_id
+#data = {"charge": charge}
+#response = requests.post(url, headers=headers, data=data)
+#it works...
